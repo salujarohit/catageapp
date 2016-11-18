801f5b856a11467adec1a1c96ca0e3f71a4f043a
Rohit Saluja <rohitsaluja94@gmail.com>
2016-11-18 16:54:34 +0400

    Initial Commit

---
+++ Gusessing Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gusessing Game
+//
+//  Created by Rohit Saluja on 11/18/16.
+//  Copyright © 2016 Rohit Saluja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
